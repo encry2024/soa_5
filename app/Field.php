244c533f247ec8148c38d3ec83e66cb1cb0b23ca
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2015-12-04 21:38:03 +0800

    Added field, and information model

---
+++ app/Field.php
@@ -0,0 +1,28 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Field extends Eloquent {
+
+	protected $fillable = array('student_label');
+
+    public function assessmentuser() {
+        return $this->belongsTo('AssessmentUser');
+    }
+
+    public static function add_field() {
+        if (isset($_POST["mytext"]) != '') {
+            foreach($_POST["mytext"] as $labelField) {
+                if ($labelField != '') {
+                    $field = Field::firstOrNew(array(
+                        'student_label' =>  $labelField,
+                    ));
+                    $field->save();
+                }
+            }
+        }
+
+        return Redirect::back()->with('msg', 'Field was successfuly updated');
+    }
+
+}
---
+++ app/Information.php
@@ -0,0 +1,47 @@
+<?php namespace App;
+
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+class Information extends Eloquent {
+
+   protected $table = 'information';
+
+    protected $fillable = array('student_id', 'field_id', 'value');
+
+    public function field() {
+        return $this->belongsTo('Field');
+    }
+
+    $information = Information::all();
+        $fields      = Field::all();
+
+    public static update_information(Request $requests) {
+        foreach ($information as $info) {
+            if ($info->field->student_label == "payment_date1") {
+                $info->value = $requests->get('down_payment');
+                $info->save();
+            }
+
+            if ($info->field->student_label == "payment_date2") {
+                $info->value = $requests->get('sec_payment');
+                $info->save();
+            }
+
+            if ($info->field->student_label == "payment_date3") {
+                $info->value = $requests->get('thrd_payment');
+                $info->save();
+            }
+
+            if ($info->field->student_label == "payment_date4") {
+                $info->value = $requests->get('fth_payment');
+                $info->save();
+            }
+
+            if ($info->field->student_label == "payment_date5") {
+                $info->value = $requests->get('ffth_payment');
+                $info->save();
+            }
+        }
+    }
+
+}
