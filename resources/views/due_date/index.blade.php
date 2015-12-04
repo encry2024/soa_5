25701178433634db44f3678b279657bf04a29951
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2015-12-04 21:44:17 +0800

    Added names on the five input fields

--- resources/views/due_date/index.blade.php
+++ resources/views/due_date/index.blade.php
@@ -116,7 +116,7 @@
                             <div class="field">
                                 <label>Down Payment Date</label>
                                 <div class="ui input left icon">
-                                    <input class="datepicker_1" type="text" placeholder="Down Payment Date">
+                                    <input name="due_1" class="datepicker_1" type="text" placeholder="Down Payment Date">
                                     <i class="calendar icon"></i>
                                 </div>
                             </div>
@@ -124,7 +124,7 @@
                             <div class="field">
                                 <label>2nd Payment *</label>
                                 <div class="ui left icon input">
-                                    <input class="datepicker_2" type="text" placeholder="2nd Payment Date">
+                                    <input name="due_2" class="datepicker_2" type="text" placeholder="2nd Payment Date">
                                     <i class="calendar icon"></i>
                                 </div>
                             </div>
@@ -132,7 +132,7 @@
                             <div class="field">
                                 <label>3rd Payment *</label>
                                 <div class="ui left icon input">
-                                    <input class="datepicker_3" type="text" placeholder="3rd Payment Date">
+                                    <input name="due_3" class="datepicker_3" type="text" placeholder="3rd Payment Date">
                                     <i class="calendar icon"></i>
                                 </div>
                             </div>
@@ -140,7 +140,7 @@
                             <div class="field">
                                 <label>4th Payment *</label>
                                 <div class="ui left icon input">
-                                    <input class="datepicker_4" type="text" placeholder="4th Payment Date">
+                                    <input name="due_4" class="datepicker_4" type="text" placeholder="4th Payment Date">
                                     <i class="calendar icon"></i>
                                 </div>
                             </div>
@@ -148,7 +148,7 @@
                             <div class="field">
                                 <label>5th Payment *</label>
                                 <div class="ui left icon input">
-                                    <input class="datepicker_5" type="text" placeholder="5th Payment Date">
+                                    <input name="due_5" class="datepicker_5" type="text" placeholder="5th Payment Date">
                                     <i class="calendar icon"></i>
                                 </div>
                             </div>
