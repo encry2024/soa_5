e22a3222da8ed2d3d83f0fe264d605c9ddc2f206
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2015-12-04 22:12:40 +0800

    Removed Request in updateDueDate parameter

--- app/Information.php
+++ app/Information.php
@@ -12,7 +12,7 @@ class Information extends Eloquent {
         return $this->belongsTo('Field');
     }
 
-    public static function updateDueDate(Request $requests)
+    public static function updateDueDate($requests)
     {
         $information = Information::all();
 
