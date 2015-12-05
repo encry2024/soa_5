5aba03ada3c6963f24c03467ebc794bba87bd57e
Christan Jake Gatchalian <christan.jake2025@yahoo.com>
2015-12-05 15:55:49 +0800

    Removed due_date route, and route group. Added index on information resource only group

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -17,23 +17,16 @@ Route::bind('information', function ($id) { return App\Information::whereId($id)
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 Route::get('home', ['as' => 'home', 'uses' => 'HomeController@index']);
 
+// AUTH CONTROLLERS
 Route::controllers([
     'auth' => 'Auth\AuthController',
     'password' => 'Auth\PasswordController',
 ]);
 
-
+// USER
 Route::resource('user', 'UserController');
-
 post('update/{user_id}/role', ['as' => 'update_role', 'uses' => 'UserController@updateRoles']);
 
 
-Route::group(['middleware' => 'auth'], function () {
-
-    get('set/due_date', ['as' => 'due_dates', 'uses' => function () {
-        return view('due_date.index');
-    }]);
-    
-});
-
-Route::resource('information', 'InformationController', ['only' => ['store', 'update']]);
+// INFORMATION
+Route::resource('information', 'InformationController', ['only' => ['store', 'update', 'index']]);
